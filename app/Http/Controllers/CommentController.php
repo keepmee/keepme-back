49c6322c030d7df7a25eb91797bccd1fb6c1de22
Sofiane Akbly <sofiane.akbly@gmail.com>
2019-09-25 11:07:56 +0200

    Add store function

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers;
+use App\Services\Utils\ResponseService as Response;
+use App\Services\CommentService;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function store(CommentService $service)
+    {
+        return Response::unknown($service->store(\Auth::user(), Helpers::getRequestData(\request())));
+    }
 }
