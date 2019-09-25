d8008beb25a1a33d1d85c9c1abdfa3eb4ff354a6
Sofiane Akbly <sofiane.akbly@gmail.com>
2019-09-25 11:09:13 +0200

    Add functions

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -10,8 +10,31 @@ class NotificationController extends Controller
 {
     public function all(NotificationService $service)
     {
-        return is_array($result = $service->all(\Auth::user()))
-            ? Response::error($result['code'], $result['message'])
-            : Response::success(['notification' => $result]);
+        return Response::unknown($service->all(\Auth::user()));
+    }
+
+    public function limit(NotificationService $service)
+    {
+        return Response::unknown($service->limit(\Auth::user()));
+    }
+
+    public function read(NotificationService $service)
+    {
+        return Response::unknown($service->read(\Auth::user()));
+    }
+
+    public function unread(NotificationService $service, $type)
+    {
+        return Response::unknown($service->unread(\Auth::user(), $type));
+    }
+
+    public function accept(NotificationService $service, $id)
+    {
+        return Response::unknown($service->accept(\Auth::user(), $id));
+    }
+
+    public function deny(NotificationService $service, $id)
+    {
+        return Response::unknown($service->deny(\Auth::user(), $id));
     }
 }
