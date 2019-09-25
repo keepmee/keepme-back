c0aa1095d95ef4a84e21275febe98872fb4f0fbd
Sofiane Akbly <sofiane.akbly@gmail.com>
2019-09-25 11:22:05 +0200

    Store to db

--- app/Services/KoopApplicationService.php
+++ app/Services/KoopApplicationService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 
 
 use App\Models\Koop;
+use App\Models\KoopApplication;
 use App\Models\Nanny;
 use App\Notifications\KoopApplicationNotification;
 use App\Services\Utils\ReturnServices;
@@ -25,14 +26,19 @@ class KoopApplicationService
         if (($koop = Koop::whereId($id)->first()) === null || ($parent = User::whereId($koop->getAttributeValue('user_id'))->first()) === null)
             return ReturnServices::notFound();
         $notifications = collect($parent->notifications);
-        if ($notifications->filter(function ($notification) use ($parent, $nanny, $koop) {
+        /*if ($notifications->filter(function ($notification) use ($parent, $nanny, $koop) {
                 return $notification->type === "App\\Notifications\\KoopApplicationNotification"
                     && $notification->data['parent']['id'] === $parent->id
                     && $notification->data['nanny']['id'] === $nanny->id
                     && $notification->data['koop']['id'] === $koop->id;
-            })->count() > 0)
+            })->count() > 0)*/
+        if (($koopApplication = KoopApplication::whereNannyId($nanny->id)->whereKoopId($koop->id)->first()) !== null)
             return ReturnServices::badRequest("Vous avez déjà postulé à cette annonce");
-        $parent->notify(new KoopApplicationNotification($parent, $nanny, $koop));
+        $parent->notify(new KoopApplicationNotification($koop, $nanny));//$parent, $nanny, $koop));
+        KoopApplication::create([
+            "nanny_id" => $nanny->id,
+            "koop_id"  => $koop->id
+        ]);
         return $parent->notifications;
     }
 
